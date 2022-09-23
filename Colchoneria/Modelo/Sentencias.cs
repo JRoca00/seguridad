5c64095eba5414a1814acf0705b45f241a3cbacb
leoneldc <leonelchavez2013@hotmail.com>
2022-09-22 19:04:53 -0600

    modificación en los nombres de la tabla y tipos de funciones

--- Colchoneria/Modelo/Sentencias.cs
+++ Colchoneria/Modelo/Sentencias.cs
@@ -14,11 +14,11 @@ namespace Modelo
 
         public string[] queryLogin(string user)
         {
-
             string[] Campos = new string[300];
             string[] auto = new string[300];
             int i = 0;
-            string sql = "SELECT pk_id_usuario, username_usuario, password_usuario FROM Usuarios where username_usuario='" + user + "';";
+            string datos = "pk_id_usuario, username_usuario, password_usuario";
+            string sql = "SELECT " + datos + " FROM tbl_usuarios where username_usuario='" + user + "';";
             try
             {
                 OdbcCommand command = new OdbcCommand(sql, con.conexion());
@@ -37,10 +37,124 @@ namespace Modelo
         public void insertBitacora(string values)
         {
             string campos = "fk_id_usuario, fk_id_aplicacion, fecha_bitacora, hora_bitacora, host_bitacora, ip_bitacora, accion_bitacora";
-            string sql = "INSERT INTO BitacoraDeEventos (" + campos + ") values (" + values + ");";
+            string sql = "INSERT INTO tbl_bitacoraDeEventos (" + campos + ") values (" + values + ");";
             OdbcCommand cmd = new OdbcCommand(sql, con.conexion());
             cmd.ExecuteNonQuery();
         }
+
+        public string queryNombreApp(string app)
+        {
+            string nombreApp = "";
+            string sql = "SELECT nombre_aplicacion FROM tbl_aplicaciones where pk_id_aplicacion='" + app + "';";
+            try
+            {
+                OdbcCommand command = new OdbcCommand(sql, con.conexion());
+                OdbcDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    nombreApp = reader.GetValue(0).ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString() + " \nError en obtener el nombre de la aplicación");
+            }
+            return nombreApp;
+        }
+
+        public int[] getPerfilesUsuario(string user)
+        {
+            int[] perfiles = new int[100];
+            int i = 0;
+            string sql = "SELECT fk_id_perfil FROM tbl_asignacionesPerfilsUsuario WHERE fk_id_usuario='" + user + "';";
+            try
+            {
+                OdbcCommand command = new OdbcCommand(sql, con.conexion());
+                OdbcDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    perfiles[i] = int.Parse(reader.GetValue(0).ToString());
+                    i++;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString() + " \nError en obtener el nombre de la aplicación");
+            }
+
+            return perfiles;
+        }
+
+        public int[] getPerfilAplicacion(int perfil)
+        {
+            int[] modulos = new int[300];
+            int i = 0;
+            string sql = "SELECT fk_id_aplicacion FROM tbl_permisosAplicacionPerfil WHERE fk_id_perfil='" + perfil + "';";
+            try
+            {
+                OdbcCommand command = new OdbcCommand(sql, con.conexion());
+                OdbcDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    modulos[i] = int.Parse(reader.GetValue(0).ToString());
+                    i++;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString() + " \nError en obtener las aplicaciones del perfil");
+            }
+            return modulos;
+        }
+
+        public int getModuloAplicacion(int aplicacion)
+        {
+            int idModulo = 0;
+            int i = 0;
+            string sql = "SELECT fk_id_modulos FROM tbl_AsignacionModuloAplicacion where fk_id_aplicacion='" + aplicacion + "';";
+            try
+            {
+                OdbcCommand command = new OdbcCommand(sql, con.conexion());
+                OdbcDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    idModulo = int.Parse(reader.GetValue(0).ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString() + " \nError en obtener el modulo de la aplicacion #" + aplicacion);
+            }
+
+            return idModulo;
+        }
+
+        public int[] getPermisos(int perfil, int aplicacion)
+        {
+            int[] permisos = new int[5];
+            int i = 0;
+            string campos = "guardar_permiso, modificar_permiso, eliminar_permiso, buscar_permiso, imprimir_permiso";
+            string sql = "SELECT " + campos + " FROM tbl_permisosAplicacionPerfil WHERE fk_id_perfil='" + perfil + "' AND " + aplicacion + ";";
+            try
+            {
+                OdbcCommand command = new OdbcCommand(sql, con.conexion());
+                OdbcDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    permisos[i] = int.Parse(reader.GetValue(0).ToString());
+                    permisos[i + 1] = int.Parse(reader.GetValue(1).ToString());
+                    permisos[i + 2] = int.Parse(reader.GetValue(2).ToString());
+                    permisos[i + 3] = int.Parse(reader.GetValue(3).ToString());
+                    permisos[i + 4] = int.Parse(reader.GetValue(4).ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString() + " \nError en obtener las aplicaciones del perfil");
+            }
+            return permisos;
+        }
+
 
         public OdbcDataAdapter buscarlogin(string tabla, string dato1, string dato2)
         {
@@ -88,7 +202,6 @@ namespace Modelo
             cmd.ExecuteNonQuery();
 
         }
-
 
         public void eliminar(string tabla,string condicion,int campo)
         {
